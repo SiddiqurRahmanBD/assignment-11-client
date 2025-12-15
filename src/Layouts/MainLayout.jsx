106f03312e610ba6b8ff31c52ca2fc73c868ecaa
Siddiqur Rahman <mdsqkn@gmail.com>
2025-12-16 00:40:58 +0600

    Added Dashboard

--- src/Layouts/MainLayout.jsx
+++ src/Layouts/MainLayout.jsx
@@ -2,17 +2,30 @@
 import { Outlet } from 'react-router';
 import Navbar from '../Components/Navbar';
 import Footer from '../Components/Footer';
+import { ToastContainer } from 'react-toastify';
 
 const MainLayout = () => {
     return (
       <div>
         <Navbar></Navbar>
-        
+
         <div className="pt-24 min-h-[calc(100vh-68px)]">
-             <Outlet></Outlet>
+          <Outlet></Outlet>
         </div>
-       
-      <Footer></Footer>
+
+        <Footer></Footer>
+        <ToastContainer
+          position="top-right"
+          autoClose={3000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick={false}
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="light"
+        />
       </div>
     );
 };
--- src/Pages/Home/RegisterPage/Register.jsx
+++ src/Pages/Home/RegisterPage/Register.jsx
@@ -1,153 +1,285 @@
-import toast from 'daisyui/components/toast';
-import React, { useContext, useState } from 'react';
-import { Link, useNavigate } from 'react-router';
-import { AuthContext } from '../../../Provider/AuthContext';
-import { IoEyeOff } from 'react-icons/io5';
-import { FaEye } from 'react-icons/fa';
+
+import React, { useContext, useEffect, useState } from "react";
+import {Link, useNavigate } from "react-router";
+import { AuthContext } from "../../../Provider/AuthContext";
+import { IoEyeOff } from "react-icons/io5";
+import { FaEye } from "react-icons/fa";
+import axios from "axios";
+import { toast } from "react-toastify";
 
 const Register = () => {
+  const [nameError, setNameError] = useState("");
+  const [passwordError, setPasswordError] = useState("");
+  const [confirmPasswordError, setConfirmPasswordError] = useState("");
 
-     const [nameError, setNameError] = useState("");
-     const [passwordError, setPasswordError] = useState("");
-     const [show, setShow] = useState(false);
+  const [show, setShow] = useState(false);
 
-     const navigate = useNavigate();
-     const { createUser, setUser, updateUser } =
-       useContext(AuthContext);
+  const navigate = useNavigate();
+  const { createUser, setUser, updateUser } = useContext(AuthContext);
 
-     const handleRegister = (e) => {
-       e.preventDefault();
+  const [districts, setDistricts] = useState([])
+  const [upzilas, setUpzilas] = useState([])
 
-       const form = e.target;
-       const name = form.name.value;
+  const [district, setDistrict] = useState('')
+  const [upzila, setUpzila] = useState('')
+ 
+  
+useEffect(() => {
+  axios
+    .get("/district.json")
+    .then((res) => {
+      setDistricts(res.data);
+    })
+    .catch((err) => console.log("District error:", err));
 
-       if (name.length < 5) {
-         setNameError("Use more than 5 Characters");
-         return;
-       } else {
-         setNameError("");
-       }
+  axios
+    .get("/upzila.json")
+    .then((res) => {
+      setUpzilas(res.data);
+    })
+    .catch((err) => console.log("Upzila error:", err));
+}, []);
 
-       const photo = form.photo.value;
-       const email = form.email.value;
-       const emailRegEx = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
-       if (!emailRegEx.test(email)) {
-         console.log("Invalid email");
-         return;
-       }
+  const handleRegister = async (e) => {
+    e.preventDefault();
 
-       const password = form.password.value;
-       const regex = /^(?=.*[a-z])(?=.*[A-Z]).{6,}$/;
+    const form = e.target;
+    const name = form.name.value;
+    const photo = form.photo;
+    const file = photo.files[0];
+    const email = form.email.value;
+    const password = form.password.value;
+    const confirmPassword = form.confirmPassword.value;
+    const bloodGroup = form.bloodGroup.value;
 
-       if (!regex.test(password)) {
-         setPasswordError(
-           "Password must have at least one uppercase letter, one lowercase letter, and be at least 6 characters long."
-         );
-         return;
-       } else {
-         setPasswordError("");
-       }
+    if (name.length < 5) {
+      setNameError("Use more than 5 Characters");
+      return;
+    } else {
+      setNameError("");
+    }
 
-       createUser(email, password)
-         .then((result) => {
-           const user = result.user;
-           toast.success("Registered Successfully!");
+    const emailRegEx = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
-           updateUser({ displayName: name, photoURL: photo })
-             .then(() => {
-               setUser({ ...user, displayName: name, photoURL: photo });
-               navigate("/");
-             })
-             .catch((error) => console.log(error));
-         })
-         .catch((error) => toast.error(error.code));
-     };
+    if (!emailRegEx.test(email)) {
+      console.log("Invalid email");
+      return;
+    }
 
-    return (
-      <div>
-        <title>Register</title>
+    const regex = /^(?=.*[a-z])(?=.*[A-Z]).{6,}$/;
 
-        <div className="hero py-10 min-h-screen">
-          <div className="card bg-base-100 w-full max-w-sm shadow-2xl">
-            <h1 className="text-2xl text-center pt-5 font-semibold">
-              Register Now
-            </h1>
+    if (!regex.test(password)) {
+      setPasswordError(
+        "Password must have at least one uppercase letter, one lowercase letter, and be at least 6 characters long."
+      );
+      return;
+    } else {
+      setPasswordError("");
+    }
 
-            <div className="card-body">
-              <form onSubmit={handleRegister}>
-                <fieldset className="fieldset">
-                  <label className="label text-lg">Name</label>
+    if (password !== confirmPassword) {
+      setConfirmPasswordError("Passwords do not match");
+      return;
+    } else {
+      setConfirmPasswordError("");
+    }
+
+
+
+    const res = await axios.post(
+      `https://api.imgbb.com/1/upload?key=80872c72797ec82a69fc4e1b1174a045`,
+      { image: file },
+      {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      }
+    );
+    const photoURL = res.data.data.display_url;
+
+    const formdata = {
+      name,
+      email,
+      bloodGroup,
+      photoURL,
+      district,
+      upzila
+    };
+    console.log(formdata);
+
+    createUser(email, password)
+      .then(() => {
+        // const user = result.user;
+        toast.success("Registered Successfully!");
+
+        updateUser({ displayName:name, photoURL: photoURL })
+          .then(() => {
+            axios.post("http://localhost:3000/users", formdata);
+            // setUser({ ...user, displayName: name, photoURL: photo });
+            navigate("/");
+          })
+          .catch((error) => console.log(error));
+      })
+      .catch((error) => toast.error(error.code));
+
+  };
+
+  return (
+    <div>
+      <title>Register</title>
+
+      <div className="hero py-10 min-h-screen">
+        <div className="card bg-base-100 w-full max-w-sm shadow-2xl">
+          <h1 className="text-2xl text-center pt-5 font-semibold">
+            Register Now
+          </h1>
+
+          <div className="card-body">
+            <form onSubmit={handleRegister}>
+              <fieldset className="fieldset">
+                <label className="label text-lg">Name</label>
+                <input
+                  type="text"
+                  name="name"
+                  className="input"
+                  placeholder="Enter Your Full Name"
+                  required
+                />
+                {nameError && (
+                  <p className="text-xs text-red-500">{nameError}</p>
+                )}
+
+                <label className="label text-lg">Photo URL</label>
+                <input
+                  type="file"
+                  name="photo"
+                  className="input"
+                  placeholder="Photo URL"
+                  required
+                />
+
+                <label className="label text-lg">Blood Group</label>
+
+                <select
+                  name="bloodGroup"
+                  className="select select-bordered "
+                  required
+                  defaultValue=""
+                >
+                  <option value="" disabled>
+                    Select your blood group
+                  </option>
+                  <option value="A+">A+</option>
+                  <option value="A-">A-</option>
+                  <option value="B+">B+</option>
+                  <option value="B-">B-</option>
+                  <option value="AB+">AB+</option>
+                  <option value="AB-">AB-</option>
+                  <option value="O+">O+</option>
+                  <option value="O-">O-</option>
+                </select>
+
+                <select
+                  value={district}
+                  onChange={(e) => setDistrict(e.target.value)}
+                  className="select select-bordered "
+                  defaultValue=""
+                >
+                  <option value="" disabled>
+                    Select your District
+                  </option>
+                  {districts.map((d) => (
+                    <option value={d?.name} key={d?.id}>
+                      {d?.name}
+                    </option>
+                  ))}
+                </select>
+                <select
+                  value={upzila}
+                  onChange={(e) => setUpzila(e.target.value)}
+                  className="select select-bordered "
+                  defaultValue=""
+                >
+                  <option value="" disabled>
+                    Select your Upzila
+                  </option>
+                  {upzilas.map((u) => (
+                    <option value={u?.name} key={u?.id}>
+                      {u?.name}
+                    </option>
+                  ))}
+                </select>
+
+                <label className="label text-lg">Email</label>
+                <input
+                  type="email"
+                  name="email"
+                  autoComplete="off"
+                  className="input"
+                  placeholder="Enter Your Email Address"
+                  required
+                />
+
+                <div className="relative">
+                  <label className="label text-lg">Password</label>
                   <input
-                    type="text"
-                    name="name"
+                    type={show ? "text" : "password"}
+                    name="password"
+                    autoComplete="new-password"
                     className="input"
-                    placeholder="Enter Your Full Name"
+                    placeholder="Enter Your Password"
                     required
                   />
-                  {nameError && (
-                    <p className="text-xs text-red-500">{nameError}</p>
+                  {passwordError && (
+                    <p className="text-xs text-red-500">{passwordError}</p>
                   )}
-
-                  <label className="label text-lg">Photo URL</label>
-                  <input
-                    type="text"
-                    name="photo"
-                    className="input"
-                    placeholder="Photo URL"
-                    required
-                  />
-
-                  <label className="label text-lg">Email</label>
-                  <input
-                    type="email"
-                    name="email"
-                    autoComplete="off"
-                    className="input"
-                    placeholder="Enter Your Email Address"
-                    required
-                  />
-
-                  <div className="relative">
-                    <label className="label text-lg">Password</label>
-                    <input
-                      type={show ? "text" : "password"}
-                      name="password"
-                      autoComplete="new-password"
-                      className="input"
-                      placeholder="Enter Your Password"
-                      required
-                    />
-                    {passwordError && (
-                      <p className="text-xs text-red-500">{passwordError}</p>
-                    )}
-                    <span
-                      onClick={() => setShow(!show)}
-                      className="absolute right-[25px] top-[42px] cursor-pointer z-50"
-                    >
-                      {show ? <FaEye size={15} /> : <IoEyeOff size={15} />}
-                    </span>
-                  </div>
-                  <button
-                    className="btn bg-green-700 text-white mt-4"
-                    type="submit"
+                  <span
+                    onClick={() => setShow(!show)}
+                    className="absolute right-[25px] top-[42px] cursor-pointer z-50"
                   >
-                    Register
-                  </button>
-                </fieldset>
-              </form>
+                    {show ? <FaEye size={15} /> : <IoEyeOff size={15} />}
+                  </span>
+                </div>
 
-              <p className="text-center">
-                Already have an account?{" "}
-                <Link className="text-green-500" to="/auth/login">
-                  Login
-                </Link>
-              </p>
-            </div>
+                <div className="relative">
+                  <label className="label text-lg">Confirm Password</label>
+                  <input
+                    type={show ? "text" : "password"}
+                    name="confirmPassword"
+                    autoComplete="new-password"
+                    className="input"
+                    placeholder="Confirm Your Password"
+                    required
+                  />
+
+                  {confirmPasswordError && (
+                    <p className="text-xs text-red-500">
+                      {confirmPasswordError}
+                    </p>
+                  )}
+                </div>
+
+                <button
+                  className="btn btn-primary text-white hover:bg-orange-400 hover:text-black mt-4"
+                  type="submit"
+                >
+                  Register
+                </button>
+              </fieldset>
+            </form>
+
+            <p className="text-center">
+              Already have an account?{" "}
+              <Link className="text-green-500" to="/auth/login">
+                Login
+              </Link>
+            </p>
           </div>
         </div>
       </div>
-    );
+    </div>
+  );
 };
 
 export default Register;
